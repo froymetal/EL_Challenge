556ff04f5d0f4f67447c92a0977172f47a88862d
Froy <54004493+froymetal@users.noreply.github.com>
2022-07-14 09:49:37 -0500

    adding user image

--- EL_Challenge/Challenge2/Challenge2ViewController.swift
+++ EL_Challenge/Challenge2/Challenge2ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class Challenge2ViewController: UIViewController {
 
     @IBOutlet weak var colorSegmentControl: UISegmentedControl!
-    
+    @IBOutlet weak var userImage: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,16 +25,22 @@ class Challenge2ViewController: UIViewController {
         switch itemSelected {
         case 0:
             colorSegmentControl.selectedSegmentTintColor = UIColor.red
+            userImage.tintColor = UIColor.red
         case 1:
             colorSegmentControl.selectedSegmentTintColor = UIColor.blue
+            userImage.tintColor = UIColor.blue
         case 2:
             colorSegmentControl.selectedSegmentTintColor = UIColor.orange
+            userImage.tintColor = UIColor.orange
         case 3:
             colorSegmentControl.selectedSegmentTintColor = UIColor.yellow
+            userImage.tintColor = UIColor.yellow
         case 4:
             colorSegmentControl.selectedSegmentTintColor = UIColor.purple
+            userImage.tintColor = UIColor.purple
         case 5:
             colorSegmentControl.selectedSegmentTintColor = UIColor.gray
+            userImage.tintColor = UIColor.gray
         default:
             print("Error")
         }
