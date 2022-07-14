1e96c2dedf9298c960784cbd5a6a9aa7e55d20fb
Froy <54004493+froymetal@users.noreply.github.com>
2022-07-14 17:33:17 -0500

    Challenge 5 Uikit code

---
+++ EL_Challenge/Challenge5/Challenge5UIKITViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Challenge5UIKITViewController.swift
+//  EL_Challenge
+//
+//  Created by Froy on 7/14/22.
+//
+
+import UIKit
+
+class Challenge5UIKITViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func closeScreen(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ EL_Challenge/Challenge5/Challenge5ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge5ViewController.swift
+//  EL_Challenge
+//
+//  Created by Froy on 7/14/22.
+//
+
+import UIKit
+
+class Challenge5ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
