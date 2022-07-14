4e5d30faa1a021d1b39268a2c83ad592f7c64638
Froy <54004493+froymetal@users.noreply.github.com>
2022-07-14 07:21:35 -0500

    Initial Commit

---
+++ EL_Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EL_Challenge
+//
+//  Created by Field Employee on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
